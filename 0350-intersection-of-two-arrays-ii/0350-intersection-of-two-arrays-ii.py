59ec8750e3bdcf57af61e84ed75da0dcddd53839
Mahammad <64745140+mahammadverdiyev@users.noreply.github.com>
2024-09-30 03:19:10 +0400

    0350-INTERSECTION-OF-TWO-ARRAYS-II Stats: Time: 48 ms (58.63%), Space: 16.9 MB (9.14%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -2,12 +2,21 @@ class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         d = dict()
         
-        for n in nums1:
+        # optimizing memory
+        small = nums1 if len(nums1) < len(nums2) else nums2
+        big = nums1 if len(nums1) > len(nums2) else nums2
+
+        if len(nums1) == len(nums2):
+            small = nums1
+            big = nums2
+
+
+        for n in small:
             d[n] = d.get(n,0) + 1
 
-        
+
         res = []
-        for n in nums2:
+        for n in big:
             if d.get(n,0) != 0:
                 res.append(n)
                 d[n] = d.get(n) - 1
